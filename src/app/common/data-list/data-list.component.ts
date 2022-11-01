19f6e90b867b50180a52f093f658738234b6a211
kovacsav <mezbogyo@gmail.com>
2022-11-01 09:10:42 +0100

    1-6.

---
+++ src/app/common/data-list/data-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { User } from 'src/app/model/user';
+
+@Component({
+  selector: 'app-data-list',
+  templateUrl: './data-list.component.html',
+  styleUrls: ['./data-list.component.scss']
+})
+export class DataListComponent implements OnInit {
+
+  dataRow: User = new User;
+
+  @Input() dataList: User[] = [];
+
+  @Output() selectClick: EventEmitter<User> = new EventEmitter();
+  @Output() updateClick: EventEmitter<User> = new EventEmitter();
+  @Output() deleteClick: EventEmitter<User> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelectClick(): void {
+    this.selectClick.emit(this.dataRow);
+  }
+
+  onUpdateClick(): void {
+    this.updateClick.emit(this.dataRow);
+  }
+
+  onDeleteClick(): void {
+    this.deleteClick.emit(this.dataRow)
+  }
+
+}
